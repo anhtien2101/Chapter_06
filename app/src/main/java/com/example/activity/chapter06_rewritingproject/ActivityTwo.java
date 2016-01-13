11bcfa49c2d1c98e86935398b0b4af0c729665d2
Anh Tien <anhtien210@gmail.com>
2016-01-13 09:20:20 +0700

    first commit

---
+++ app/src/main/java/com/example/activity/chapter06_rewritingproject/ActivityTwo.java
@@ -0,0 +1,13 @@
+package com.example.activity.chapter06_rewritingproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_two);
+    }
+}
